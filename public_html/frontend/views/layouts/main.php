2d026d3da99352760206b24fff8b3ca1833687b0
asmalouski <asmaliaks@gmail.com>
2017-08-30 14:26:18 +0300

    Убраны лишние скрипты

--- public_html/frontend/views/layouts/main.php
+++ public_html/frontend/views/layouts/main.php
@@ -21,11 +21,11 @@ AppAsset::register($this);
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::decode(strip_tags($this->title)) ?></title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-    <script src="https://unpkg.com/react@15/dist/react.min.js"></script>
-    <script src="https://unpkg.com/react-dom@15/dist/react-dom.min.js"></script>
+<!--    <script src="https://unpkg.com/react@15/dist/react.min.js"></script>-->
+<!--    <script src="https://unpkg.com/react-dom@15/dist/react-dom.min.js"></script>-->
 
     <?php //$this->head() ?>
-    <?= frontend\widgets\Frontend::widget()?>
+    <?// frontend\widgets\Frontend::widget()?>
     <!-- Latest compiled and minified CSS -->
 <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.1/css/bootstrap-select.min.css">-->
 
