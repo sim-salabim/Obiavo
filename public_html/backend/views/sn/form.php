b02e340e25cdfdf615eb41c0e92cffe089b622bf
asmalouski <asmaliaks@gmail.com>
2018-02-05 16:16:38 +0300

    Улучшение сохранения СС в админке

--- public_html/backend/views/sn/form.php
+++ public_html/backend/views/sn/form.php
@@ -4,16 +4,20 @@ use backend\widgets\Form;
 $current_value = [];
 $current_value['id'] = (isset($sn->default_group_id) and $sn->default_group_id) ? $sn->default_group_id : null;
 $current_value['title'] = (isset($sn->default_group_id) and $sn->default_group_id) ? $sn->default->name : null;
+$attributes = [
+    ['name' => 'name','type' => Form::INPUT_TEXT,'label' => 'Название','model'=>$sn],
+    ['name' => 'active','type' => Form::INPUT_CHECKBOX,'label' => 'Активность','model'=>$sn, 'model_name' => 'SocialNetworks']
+];
+$default_group = null;
+if($sn->id){
+$attributes[]  = ['name' => 'default_group_id','type' => Form::SEARCH_AUTOCOMPLETE,'label' => 'Группа по умолчанию', 'model_name' => 'SocialNetworks', 'model'=>$sn, 'url' => 'sn-groups/search?sn_id='.$sn->id, 'placeholder' => 'Начните печатать название группы...', 'current_value' => $current_value, 'input_id' => 'socialnetworks-default_group_id'];
+}
 $items = [
     'saveUrl' => $toUrl,
     'rows' => [
         [
             'panel-title' => 'Основныe данные',
-            'attributes' => [
-                  ['name' => 'name','type' => Form::INPUT_TEXT,'label' => 'Название','model'=>$sn],
-                  ['name' => 'default_group_id','type' => Form::SEARCH_AUTOCOMPLETE,'label' => 'Группа по умолчанию', 'model_name' => 'SocialNetworks', 'model'=>$sn, 'url' => 'sn-groups/search', 'placeholder' => 'Начните печатать название группы...', 'current_value' => $current_value, 'input_id' => 'socialnetworks-default_group_id'],
-                ['name' => 'active','type' => Form::INPUT_CHECKBOX,'label' => 'Активность','model'=>$sn, 'model_name' => 'SocialNetworks'],
-            ]
+            'attributes' => $attributes
         ]
     ]
 ];
