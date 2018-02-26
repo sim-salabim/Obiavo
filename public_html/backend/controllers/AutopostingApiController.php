6d43ec384ff14cdf2163ac3a10e85d2a14fa189d
asmalouski <asmaliaks@gmail.com>
2018-02-26 11:28:11 +0300

    Логирование убрал

--- public_html/backend/controllers/AutopostingApiController.php
+++ public_html/backend/controllers/AutopostingApiController.php
@@ -28,14 +28,11 @@ class AutopostingApiController extends Controller
     public function actionIndex(){
         Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
         $this->checkCredentials();
-        file_get_contents('https://api.telegram.org/bot517180739:AAG_ZzuRtwArLMOeX7xEXYP9NXoEJIasPnk/sendMessage?text="credentials valid"&chat_id=88740047');
         $social_networks = SocialNetworks::find()->where(['autoposting' => 1])->all();
-        file_get_contents('https://api.telegram.org/bot517180739:AAG_ZzuRtwArLMOeX7xEXYP9NXoEJIasPnk/sendMessage?text="Соцсетей с автопостингом '.count($social_networks).'"&chat_id=88740047');
         if(count($social_networks)){
             foreach ($social_networks as $sn){
                 $task = $sn->getActiveAutopostingTask();
                 if($task){
-                    file_get_contents('https://api.telegram.org/bot517180739:AAG_ZzuRtwArLMOeX7xEXYP9NXoEJIasPnk/sendMessage?text="Задача найдена"&chat_id=88740047');
                     switch ($sn->name){
                         case SocialNetworks::VK_COM :
                             (new AutopostingVk($task))->post();
--- public_html/common/models/libraries/AutopostingVk.php
+++ public_html/common/models/libraries/AutopostingVk.php
@@ -28,7 +28,6 @@ class AutopostingVk {
     function post(){
         $attachements = '';
         if(count($this->task->ad->files)){
-            file_get_contents('https://api.telegram.org/bot517180739:AAG_ZzuRtwArLMOeX7xEXYP9NXoEJIasPnk/sendMessage?text="есть файлы у обьявления '.count($this->task->ad->files).'"&chat_id=88740047');
             $album = $this->createAlbumIfNotExists();
             $photos_uploaded = [];
             if($album) $photos_uploaded = $this->uploadPhotos($album->id);
@@ -44,9 +43,7 @@ class AutopostingVk {
         $api_request_str = str_replace('{endpoint:key}', self::ENDPOINT_WALL_POST, $this->api_url);
         $api_request_str .= '&from_group=1&owner_id=-'.$this->task->socialNetworksGroup->group_id.'&message='.urlencode($this->task->ad->text).'&'.$attachements;
         $result = json_decode(file_get_contents($api_request_str));
-        file_get_contents('https://api.telegram.org/bot517180739:AAG_ZzuRtwArLMOeX7xEXYP9NXoEJIasPnk/sendMessage?text="try to post"&chat_id=88740047');
         if(isset($result->error)){
-            file_get_contents('https://api.telegram.org/bot517180739:AAG_ZzuRtwArLMOeX7xEXYP9NXoEJIasPnk/sendMessage?text="error '.$result->error->error_msg.'"&chat_id=88740047');
             Mailer::send(\Yii::$app->params['debugEmail'], "Ошибка API VK.COM", 'api-error', ['error' => $result->error->error_msg, 'request' => $api_request_str, 'message' => 'Ошибка получения сервера для загрузки фото']);
             $this->task->status = AutopostingTasks::STATUS_FAILED;
             $this->task->save();
