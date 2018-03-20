9b54f3997b2cca68667f044e8832f95f78f10887
asmalouski <asmaliaks@gmail.com>
2018-03-20 11:02:37 +0300

    Публикация фото для твитер

--- public_html/common/models/libraries/AutopostingTwitter.php
+++ public_html/common/models/libraries/AutopostingTwitter.php
@@ -4,8 +4,12 @@ namespace common\models\libraries;
 use Abraham\TwitterOAuth\TwitterOAuth as TwitterAuth;
 use common\models\AutopostingTasks;
 use common\models\Mailer;
+use yii\base\Exception;
 
 class AutopostingTwitter {
+
+    const FILES_LIMIT = 4;
+    const FILE_SIZE_LIMIT = 5000000;
 
     private $task;
     private $group;
@@ -16,9 +20,31 @@ class AutopostingTwitter {
     }
 
     function post(){
-
         $connection = new TwitterAuth($this->group->consumer_key, $this->group->consumer_secret, $this->group->group_id, $this->group->token);
-        $statuses = $connection->post("statuses/update", array("status" => $this->task->ad->title));
+        $task_files = $this->task->ad->files;
+        $media_ids = '';
+        $media_amount = 0;
+        // зальем файлы если такие есть и подхотят по требованиям
+        foreach($task_files as $file){
+            if($media_amount < self::FILES_LIMIT AND filesize (\Yii::$app->params['uploadPath']."/".$file->hash) < self::FILE_SIZE_LIMIT){
+                try {
+                    $media = $connection->upload("media/upload", array('media' => \Yii::$app->params['uploadPath'] . "/" . $file->hash));
+                }catch(Exception $e){
+                    TelegrammLoging::send('Ошибка размещения медиафайлов в Twitter  ID сообщества: '.$this->task->socialNetworksGroup->id);
+                    unset($media);
+                }
+                if(isset($media) AND !isset($media->errors)) {
+                    if($media_ids == ''){
+                        $media_ids .= $media->media_id_string;
+                    }else{
+                        $media_ids .= ','.$media->media_id;
+                    }
+                    $media_amount++;
+                }
+            }
+        }
+
+        $statuses = $connection->post("statuses/update", array("status" => $this->task->ad->title, 'media_ids' => $media_ids));
         if($connection->getLastHttpCode() != 200){
             TelegrammLoging::send('Ошибка публикации в Twitter  ID сообщества: '.$this->task->socialNetworksGroup->id.' '.$statuses->errors[0]->message);
             Mailer::send(\Yii::$app->params['debugEmail'], "Ошибка API Twitter", 'api-error', ['message' =>$statuses->errors[0]->message]);
