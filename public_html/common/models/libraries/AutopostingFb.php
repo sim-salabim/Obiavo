8c1ac2ffc41acc2b4e4fde0a319b2ef16a26ecd8
asmalouski <asmaliaks@gmail.com>
2018-04-12 12:27:32 +0300

    Публикация обьявлений в группы ФБ.правка

--- public_html/common/models/libraries/AutopostingFb.php
+++ public_html/common/models/libraries/AutopostingFb.php
@@ -44,17 +44,16 @@ class AutopostingFb {
             'published' => true,
             'link' => $this->task->ad->city->region->country->domain.'/'.$this->task->ad->url()
         ];
-//        $ch = curl_init();
-//
-//        curl_setopt($ch, CURLOPT_URL, "https://graph.facebook.com/{$this->group_id}/feed");
-//        curl_setopt($ch, CURLOPT_HEADER, 0);
-//        curl_setopt($ch, CURLOPT_HTTPHEADER,  array("Content-Type:multipart/form-data"));
-//        curl_setopt($ch, CURLOPT_POSTFIELDS,  $postfields);
-//        curl_exec($ch);
-//        $response = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-//        curl_close($ch);
-//        if($response != 200){
-        if(true == false){
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, "https://graph.facebook.com/{$this->group_id}/feed");
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_HTTPHEADER,  array("Content-Type:multipart/form-data"));
+        curl_setopt($ch, CURLOPT_POSTFIELDS,  $postfields);
+        curl_exec($ch);
+        $response = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        if($response != 200){
             TelegrammLoging::send('Ошибка публикации на странице Facebook '.$this->task->socialNetworksGroup->group_id.' https://graph.facebook.com/'.$this->group_id.'/feed access_token => '.$this->token);
             Mailer::send(\Yii::$app->params['debugEmail'], "Ошибка API Facebook", 'api-error', [ 'request' => $this->task->socialNetworksGroup->group_id.' https://graph.facebook.com/'.$this->group_id.'/feed access_token => '.$this->token.', message =>'.$postfields['message'].', link => '.$postfields['link'], 'message' => 'Ошибка публикации на странице Facebook']);
             $this->task->status = AutopostingTasks::STATUS_FAILED;
