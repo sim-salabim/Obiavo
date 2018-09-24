0b8ca46050852e4b2dbe9cfd8086a41d8f1121e8
asmalouski <asmaliaks@gmail.com>
2018-09-24 11:04:34 +0300

    Правка баша обновления сертификатов

--- public_html/update_certs.sh
+++ public_html/update_certs.sh
@@ -1,1 +1,2 @@
-/home/YaAdmin/certbot-auto renew --pre-hook "service nginx stop" --post-hook "service nginx start"
+#/home/YaAdmin/certbot-auto renew --pre-hook "service nginx stop" --post-hook "service nginx start"
+service nginx stop & /home/YaAdmin/certbot-auto renew & service nginx start
