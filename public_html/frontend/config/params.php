03d1cbd61d3ccf6a81f7a3ab790d03d1c41e18e4
Сервер 36n6.ru <36n6@mail.ru>
2018-10-29 10:41:13 +0100

    Gitignore & params

--- public_html/frontend/config/params.php
+++ public_html/frontend/config/params.php
@@ -3,6 +3,7 @@ return [
     'commonAdminEmail' => 'mail@obiavo.ru',
     'technicalSupportEmail' => 'webmaster@obiavo.ru',
     'mainDomain' => 'obiavo.ru',
+    'rootUrl' => 'https://obiavo.ru/',
     'basePath' => dirname(__DIR__),
     'uploadPath' => dirname(__DIR__)."/web/files",
     'FBAppId' => "576754169327687",
