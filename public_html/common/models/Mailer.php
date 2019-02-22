a6721ea7c7c16801196c0a47b425989b7e3f800e
asmalouski <asmaliaks@gmail.com>
2019-02-22 10:24:41 +0300

    Настройка мейлинга

--- public_html/common/models/Mailer.php
+++ public_html/common/models/Mailer.php
@@ -1,7 +1,11 @@
 <?php
 namespace common\models;
 
+use common\models\libraries\TelegrammLoging;
+use Exception;
+use frontend\components\Location;
 use Yii;
+use yii\helpers\Url;
 
 class Mailer {
 
@@ -13,22 +17,43 @@ class Mailer {
      * @param $from, ассоциативный массив ['email' => 'example@mail.ru', 'name' => 'example_name']
      * @param $attachement, array <File>
      */
-    public static function send($send_to, $subject, $template, $arr, $from = null){
-
-        $from_arr = [Yii::$app->params['commonAdminEmail'] => Yii::$app->name];
-        if($from){
-            $from_arr = [$from['email']  => $from['name']];
+    public static function send($send_to, $subject, $template, $arr){
+        $url = Url::base(true);
+        switch(Location::getCurrentDomain()){
+            case "obiavo.ru" :
+                $from_arr = ['robot@obiavo.ru' => Yii::$app->name];
+                break;
+            case "obiavo.by" :
+                $from_arr = ['robot@obiavo.by' => Yii::$app->name];
+                break;
+            case "obiavo.kz" :
+                $from_arr = ['robot@obiavo.kz' => Yii::$app->name];
+                break;
+            case "obiavo.uz" :
+                $from_arr = ['robot@obiavo.uz' => Yii::$app->name];
+                break;
+            case "obiavo.su" :
+                $from_arr = ['robot@obiavo.su' => Yii::$app->name];
+                break;
         }
-        $mailer = Yii::$app
-            ->mailer
-            ->compose(
-                ['html' => $template],
-                $arr
-            )
-            ->setFrom($from_arr)
-            ->setTo($send_to)
-            ->setSubject($subject);
-
-        $mailer->send();
+        // если мы на тестовом серваке, то отпрвитель должен быть robot@obiavo.site
+        if($url == "http://obiavo.site"){
+            $from_arr = ['robot@obiavo.site' => Yii::$app->name];
+        }
+        TelegrammLoging::send("From arr: ".json_encode($from_arr));
+        try {
+            Yii::$app
+                ->mailer
+                ->compose(
+                    ['html' => $template],
+                    $arr
+                )
+                ->setFrom($from_arr)
+                ->setTo($send_to)
+                ->setSubject($subject)
+                ->send();
+        }catch(Exception $e){
+            TelegrammLoging::send("Mailer send exception: ".$e->getMessage());
+        }
     }
 }
