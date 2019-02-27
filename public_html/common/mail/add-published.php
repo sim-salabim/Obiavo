9a38cc8aa84f10cb9b301446014128d158657035
asmalouski <asmaliaks@gmail.com>
2019-02-27 20:23:13 +0300

    Configs

--- public_html/common/mail/add-published.php
+++ public_html/common/mail/add-published.php
@@ -14,16 +14,19 @@
 <? }else{ ?>
     <p>
         Добро пожаловать на сайт бесплатных объявлений.</br>
-
+    </p>
+    <p>
         Ваш email: <?= $user->email ?></br>
         Ваш пароль: <?= $pass ?></br>
-
+    </p>
+    <p>
         Ваше объявление: <a href="https://<?= \frontend\components\Location::getCurrentDomain()."/".$add->url ?>/"><?= $add->title ?></a></br>
-
+    </p>
+    <p>
         Для того чтобы редактировать, поднимать или снимать с публикации ваши объявления пройдите по ссылке предварительно пройдя авторизацию с вашим электронным адресом и паролем:</br>
-
+    </p>
         <a href="https://<?= \frontend\components\Location::getCurrentDomain()."/moi-obiavleniya/" ?>"><?= __("My ads");?></a></br>
-
+    <p>
         С уважением команда Объяво</br>
         https://<?= \frontend\components\Location::getCurrentDomain() ?>
     </p>
