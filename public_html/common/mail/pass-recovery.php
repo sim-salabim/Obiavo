b31cfa60b6a371a57ef529e5420e07325abd9e47
asmalouski <asmaliaks@gmail.com>
2019-02-28 17:14:25 +0300

    Верстка письма для восстановления пароля

--- public_html/common/mail/pass-recovery.php
+++ public_html/common/mail/pass-recovery.php
@@ -2,3 +2,18 @@
     <?= $user->getFullName() ?>, для восстановления пароля пройдите по этой <br/>
     <a href="/reset?key=<?= $token ?>">ссылке</a>
 </p>
+<p>
+Здравствуйте, <?= $user->getFullName() ?>,
+</p>
+<p>
+Для восстановления пароля пройдите по ссылке ниже:
+</p>
+<p>
+<a href="https://<?= \frontend\components\Location::getCurrentDomain() ?>/reset?key=<?= $token ?>">https://<?= \frontend\components\Location::getCurrentDomain() ?>/reset?key=<?= $token ?></a>
+</p>
+<p>
+С уважением команда Объяво
+</p>
+<p>
+https://<?= \frontend\components\Location::getCurrentDomain() ?>
+</p>
