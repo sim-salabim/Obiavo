6a64d4191bfa39065c6002b636e46e0f56844412
asmalouski <asmaliaks@gmail.com>
2019-02-28 09:05:23 +0300

    Настройки мейлинга для яндекса

--- public_html/common/models/Mailer.php
+++ public_html/common/models/Mailer.php
@@ -1,7 +1,9 @@
 <?php
 namespace common\models;
 
+use frontend\components\Location;
 use Yii;
+use yii\helpers\Url;
 
 class Mailer {
 
@@ -13,13 +15,30 @@ class Mailer {
      * @param $from, ассоциативный массив ['email' => 'example@mail.ru', 'name' => 'example_name']
      * @param $attachement, array <File>
      */
-    public static function send($send_to, $subject, $template, $arr, $from = null){
-
-        $from_arr = [Yii::$app->params['commonAdminEmail'] => Yii::$app->name];
-        if($from){
-            $from_arr = [$from['email']  => $from['name']];
+    public static function send($send_to, $subject, $template, $arr){
+        $url = Url::base(true);
+        switch(Location::getCurrentDomain()){
+            case "obiavo.ru" :
+                $from_arr = ['robot@obiavo.ru' => Yii::$app->name];
+                break;
+            case "obiavo.by" :
+                $from_arr = ['robot@obiavo.by' => Yii::$app->name];
+                break;
+            case "obiavo.kz" :
+                $from_arr = ['robot@obiavo.kz' => Yii::$app->name];
+                break;
+            case "obiavo.uz" :
+                $from_arr = ['robot@obiavo.uz' => Yii::$app->name];
+                break;
+            case "obiavo.su" :
+                $from_arr = ['robot@obiavo.su' => Yii::$app->name];
+                break;
         }
-        $mailer = Yii::$app
+        // если мы на тестовом серваке, то отпрвитель должен быть robot@obiavo.site
+        if($url == "http://obiavo.site"){
+            $from_arr = ['robot@obiavo.site' => Yii::$app->name];
+        }
+        Yii::$app
             ->mailer
             ->compose(
                 ['html' => $template],
@@ -27,8 +46,8 @@ class Mailer {
             )
             ->setFrom($from_arr)
             ->setTo($send_to)
-            ->setSubject($subject);
+            ->setSubject($subject)
+            ->send();
 
-        $mailer->send();
     }
 }
