039fbf4132868a74f944dde7c7a75adb67f973ee
asmalouski <asmaliaks@gmail.com>
2019-06-14 15:24:28 +0300

    Conflicts

--- public_html/frontend/components/Location.php
+++ public_html/frontend/components/Location.php
@@ -172,7 +172,7 @@ class Location extends Component {
         if(strpos($base_url, 'obiavo.su') !== false){
             $domain = "obiavo.su";
         }
-        if(strpos($base_url, 'obiavo.com') !== false){
+        if(strpos($base_url, 'obiavo.site') !== false){
             $domain = "obiavo.com";
         }
         return $domain;
