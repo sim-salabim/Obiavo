dc05dee6c2ba4c80a73d6cb1b943d4e30738e038
asmalouski <asmaliaks@gmail.com>
2019-06-14 15:49:39 +0300

    Conflicts

--- public_html/common/models/scopes/CityQuery.php
+++ public_html/common/models/scopes/CityQuery.php
@@ -9,53 +9,53 @@ class CityQuery extends ActiveQuery {
 
     const TEXT_RELATION = 'cityText';
     const TEXT_RELATION_TABLE = 'cities_text';
-//
-//    /**
-//     * Города в текущей локации
-//     */
-//    public function byLocation(){
-//
-//        $this->joinWith(['region' => function(\yii\db\ActiveQuery $query){
-//            $query->andWhere(['regions.countries_id' => \Yii::$app->location->country->id])
-//                  ->withText();
-//        }]);
-//
-//        return $this;
-//    }
-//
-//
-//    public function whereDomain($domainName){
-//
-//        $this->andWhere(['cities.domain' => $domainName]);
-//
-//        return $this;
-//    }
-//
-//    /**
-//     * Параметр поиска
-//     * @param string $text  Строка поиска
-//     */
-//    public function search($text, $operator = 'LIKE'){
-//        $this->joinWith([
-//          'cityText ct' => function (\yii\db\ActiveQuery $query) use ($text,$operator) {
-//              $query->andWhere([$operator,'ct.name',$text])
-//                    ->onCondition(['ct.languages_id' => \Yii::$app->location->language->id]);
-//          }]);
-//
-//
-//        return $this;
-//    }
-//
-//    public function searchWithRegion($text, $operator = 'LIKE'){
-//        $this->joinWith([
-//                'cityText',
-//                'region.regionText'
-//        ]);
-//
-//        $this->andWhere([$operator,'cities_text.name',$text])->orWhere([$operator,'regions_text.name',$text]);
-//
-//
-//        return $this;
-//    }
+
+    /**
+     * Города в текущей локации
+     */
+    public function byLocation(){
+
+        $this->joinWith(['region' => function(\yii\db\ActiveQuery $query){
+            $query->andWhere(['regions.countries_id' => \Yii::$app->location->country->id])
+                  ->withText();
+        }]);
+
+        return $this;
+    }
+
+
+    public function whereDomain($domainName){
+
+        $this->andWhere(['cities.domain' => $domainName]);
+
+        return $this;
+    }
+
+    /**
+     * Параметр поиска
+     * @param string $text  Строка поиска
+     */
+    public function search($text, $operator = 'LIKE'){
+        $this->joinWith([
+          'cityText ct' => function (\yii\db\ActiveQuery $query) use ($text,$operator) {
+              $query->andWhere([$operator,'ct.name',$text])
+                    ->onCondition(['ct.languages_id' => \Yii::$app->location->language->id]);
+          }]);
+
+
+        return $this;
+    }
+
+    public function searchWithRegion($text, $operator = 'LIKE'){
+        $this->joinWith([
+                'cityText',
+                'region.regionText'
+        ]);
+
+        $this->andWhere([$operator,'cities_text.name',$text])->orWhere([$operator,'regions_text.name',$text]);
+
+
+        return $this;
+    }
 
 }
