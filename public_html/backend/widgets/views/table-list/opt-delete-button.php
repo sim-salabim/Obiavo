01c9196ae88e4c599baaabdd4dfb487235359312
asmalouski <asmaliaks@gmail.com>
2019-07-15 09:03:56 +0300

    Алерт по нажатию удаления в админке

--- public_html/backend/widgets/views/table-list/opt-delete-button.php
+++ public_html/backend/widgets/views/table-list/opt-delete-button.php
@@ -1,8 +1,21 @@
 <?php use yii\helpers\Url;?>
+<? $id = uniqid();?>
 <span data-placement="top" data-toggle="tooltip" title="Удалить">
-    <button class="btn btn-danger btn-xs senddata"
-            onclick="alert('Удалить?');"
+    <button id="<?= $id ?>" class="btn btn-danger btn-xs "
             data-link="<?= Url::toRoute(['delete','id' => $model->id])?>">
         <span class="glyphicon glyphicon-trash"></span>
     </button>
 </span>
+<script>
+    $(document).ready(function(){
+        $('#<?= $id?>').bind('click', function(e){
+            if(!$(this).hasClass('senddata')) {
+                var c = confirm('Удалить?');
+                if (c) {
+                    $(this).addClass('senddata');
+                    $(this).click();
+                }
+            }
+        });
+    });
+</script>
