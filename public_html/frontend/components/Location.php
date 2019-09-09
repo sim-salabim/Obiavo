eaa8e6a9e167010f4d3004a738883e3b365c36e0
asmalouski <asmaliaks@gmail.com>
2019-09-09 10:18:13 +0300

    Правки OpenGraph в списке

--- public_html/frontend/components/Location.php
+++ public_html/frontend/components/Location.php
@@ -176,7 +176,7 @@ class Location extends Component {
             $domain = "obiavo.com";
         }
         if(strpos($base_url, 'obiavo.site') !== false){
-            $domain = "obiavo.site";
+            $domain = "obiavo.ru";
         }
         if(strpos($base_url, 'obiavo.loc') !== false){
             $domain = "obiavo.loc";
