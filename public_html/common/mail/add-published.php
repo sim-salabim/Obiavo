e0b305479b49bbbea9ae5e1e1aa20048794e7227
asmalouski <asmaliaks@gmail.com>
2019-10-02 11:40:20 +0300

    Добвалены переводы для емейлов

--- public_html/common/mail/add-published.php
+++ public_html/common/mail/add-published.php
@@ -1,45 +1,78 @@
 <? if(!$fast){?>
+    <? if(\frontend\components\Location::getDefaultLanguageId() == \common\models\Language::LANG_EN){?>
     <p>
-        <?= _('Hello, we\'ve just posted your advertisement') ?>:</br>
+        Hello, we\'ve just posted your advertisement:</br>
     </p>
     <p>
-        <?= __('Your ad');?>: <a href="https://<?= \frontend\components\Location::getCurrentDomain()."/".$add->city->domain."/"."/".$add->url ?>/"><?= $add->title ?></a></br>
+        Your ad: <a href="https://<?= \frontend\components\Location::getCurrentDomain()."/".$add->city->domain."/"."/".$add->url ?>/"><?= $add->title ?></a></br>
     </p>
     <p>
-        <?= __('To edit, raise or inactivate your ads please get authorised using your email and password then go to the following link'); ?>:</br>
+        To edit, raise or inactivate your ads please get authorised using your email and password then go to the following link:</br>
     </p>
     <p>
-        <? if(\frontend\components\Location::getDefaultLanguageId() == \common\models\Language::LANG_RU){?>
-            <a href="https://<?= \frontend\components\Location::getCurrentDomain()."/moi-obiavleniya/" ?>"><?= __("My ads");?></a></br>
-        <? }else{?>
-            <a href="https://<?= \frontend\components\Location::getCurrentDomain()."/my-ads/" ?>"><?= __("My ads");?></a></br>
-        <? } ?>
+        <a href="https://<?= \frontend\components\Location::getCurrentDomain()."/my-ads/" ?>"><?= __("My ads");?></a></br>
     </p>
     <p>
-    <?= __('Sincerely yours Obiavo team'); ?></br>
+    Sincerely yours Obiavo team</br>
     https://<?= \frontend\components\Location::getCurrentDomain() ?>
     </p>
-<? }else{ ?>
-    <p>
-        <?= __('Welcome to our site of free ads'); ?>.</br>
-    </p>
-    <p>
-        <?= __('Your _email'); ?>: <?= $user->email ?></br>
-        <?= __('Your pass'); ?>: <?= $pass ?></br>
-    </p>
-    <p>
-        <?= __('Your ad'); ?>: <a href="https://<?= \frontend\components\Location::getCurrentDomain()."/".$add->url ?>/"><?= $add->title ?></a></br>
-    </p>
-    <p>
-        <?= __('To edit, raise or inactivate your ads please get authorised using your email and password then go to the following link'); ?>:</br>
-    </p>
-        <? if(\frontend\components\Location::getDefaultLanguageId() == \common\models\Language::LANG_RU){?>
+        <? }else{?>
+        <p>
+            Здравствуйте, вы опубликовали объявление:</br>
+        </p>
+        <p>
+            Ваше объявление: <a href="https://<?= \frontend\components\Location::getCurrentDomain()."/".$add->url ?>/"><?= $add->title ?></a></br>
+        </p>
+        <p>
+            Для того чтобы редактировать, поднимать или снимать с публикации ваши объявления пройдите по ссылке предварительно пройдя авторизацию с вашим электронным адресом и паролем:</br>
+        </p>
+        <p>
             <a href="https://<?= \frontend\components\Location::getCurrentDomain()."/moi-obiavleniya/" ?>"><?= __("My ads");?></a></br>
-        <?}else{?>
-            <a href="https://<?= \frontend\components\Location::getCurrentDomain()."/my-ads/" ?>"><?= __("My ads");?></a></br>
-        <? } ?>
+        </p>
+        <p>
+            С уважением команда Объяво</br>
+            https://<?= \frontend\components\Location::getCurrentDomain() ?>
+        </p>
+        <? }?>
+<? }else{ ?>
+    <? if(\frontend\components\Location::getDefaultLanguageId() == \common\models\Language::LANG_EN){?>
     <p>
-        <?= __('Sincerely yours Obiavo team');?></br>
+        Welcome to our site of free ads.</br>
+    </p>
+    <p>
+        Your email: <?= $user->email ?></br>
+        Your pass: <?= $pass ?></br>
+    </p>
+    <p>
+        Your ad: <a href="https://<?= \frontend\components\Location::getCurrentDomain()."/".$add->url ?>/"><?= $add->title ?></a></br>
+    </p>
+    <p>
+        To edit, raise or inactivate your ads please get authorised using your email and password then go to the following link:</br>
+    </p>
+            <a href="https://<?= \frontend\components\Location::getCurrentDomain()."/my-ads/" ?>"><?= __("My ads");?></a></br>
+
+    <p>
+        Sincerely yours Obiavo team</br>
         https://<?= \frontend\components\Location::getCurrentDomain() ?>
     </p>
+        <? }else{ ?>
+        <p>
+            Добро пожаловать на сайт бесплатных объявлений.</br>
+        </p>
+        <p>
+            Ваш email: <?= $user->email ?></br>
+            Ваш пароль: <?= $pass ?></br>
+        </p>
+        <p>
+            Ваше объявление: <a href="https://<?= \frontend\components\Location::getCurrentDomain()."/".$add->url ?>/"><?= $add->title ?></a></br>
+        </p>
+        <p>
+            Для того чтобы редактировать, поднимать или снимать с публикации ваши объявления пройдите по ссылке предварительно пройдя авторизацию с вашим электронным адресом и паролем:</br>
+        </p>
+        <a href="https://<?= \frontend\components\Location::getCurrentDomain()."/moi-obiavleniya/" ?>"><?= __("My ads");?></a></br>
+        <p>
+            С уважением команда Объяво</br>
+            https://<?= \frontend\components\Location::getCurrentDomain() ?>
+        </p>
+        <? }?>
 <? }?>
--- public_html/common/mail/pass-recovery.php
+++ public_html/common/mail/pass-recovery.php
@@ -1,15 +1,34 @@
+
+<? if(\frontend\components\Location::getDefaultLanguageId() == \common\models\Language::LANG_EN){?>
 <p>
-    <?= __("Hello")  ?>, <?= $user->getFullName() ?>.
+    Hello, <?= $user->getFullName() ?>.
 </p>
 <p>
-    <?= __("Please click to the following link to recover your password") ?>:
+    Please click to the following link to recover your password:
 </p>
 <p>
     <a href="/reset?key=<?= $token ?>"><?= "https://".\frontend\components\Location::getCurrentDomain()."/reset?key=".$token ?></a>
 </p>
 <p>
-    <?= __('Sincerely yours Obiavo team'); ?>
+    Sincerely yours Obiavo team
 </p>
 <p>
     <?= "https://".\frontend\components\Location::getCurrentDomain() ?>
 </p>
+<? }else{ ?>
+    <p>
+        <?  ?>, <?= $user->getFullName() ?>.
+    </p>
+    <p>
+        Для восстановления пароля пройдите по ссылке ниже:
+    </p>
+    <p>
+        <a href="/reset?key=<?= $token ?>"><?= "https://".\frontend\components\Location::getCurrentDomain()."/reset?key=".$token ?></a>
+    </p>
+    <p>
+        С уважением команда Объяво
+    </p>
+    <p>
+        <?= "https://".\frontend\components\Location::getCurrentDomain() ?>
+    </p>
+<? } ?>
